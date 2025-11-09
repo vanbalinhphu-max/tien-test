85a2d6143ae6034c235e46477ac1de53b4bbf40a
vanbalinhphu-max <vanbalinhphu@gmail.com>
2025-11-09 16:33:35 +0700

    fix

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,18 +1,18 @@
-import { userAgent } from "next/server";
-import sitemap from "./sitemap";
-const baseUrl =
-    process.env.NEXT_PUBLIC_BASE_URL ||
-    (process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000");
+// import { userAgent } from "next/server";
+// import sitemap from "./sitemap";
+// const baseUrl =
+//     process.env.NEXT_PUBLIC_BASE_URL ||
+//     (process.env.VERCEL_URL
+//       ? `https://${process.env.VERCEL_URL}`
+//       : "http://localhost:3000");
 
-export default function robots(){
-    return{
-        rules: {
-            userAgent: "*",
-            allow: '/',
-            disallow : ['/admin/','/private/']
-        },
-        sitemap: `${baseUrl}/sitemap.xml`
-    }
-}
+// export default function robots(){
+//     return{
+//         rules: {
+//             userAgent: "*",
+//             allow: '/',
+//             disallow : ['/admin/','/private/']
+//         },
+//         sitemap: `${baseUrl}/sitemap.xml`
+//     }
+// }
